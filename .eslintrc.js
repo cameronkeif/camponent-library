d9595dcc09d56d1b02a5891224ce63ea77681551
Cameron Keif <cameron.keif@gmail.com>
2022-01-22 13:24:32 -0600

    add config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: ['react', 'import', '@typescript-eslint'],
+  extends: ["airbnb-typescript", "plugin:storybook/recommended"],
+  rules: {
+    // Fix typescript error around "'React' was used before it was defined".
+    // See https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined/64024916#64024916
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "react/jsx-props-no-spreading": 0,
+    "react/jsx-fragments": "off",
+    "max-len": ["error", {
+      "code": 120
+    }],
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": ["**/*.test.*", "src/setupTests.ts"]
+    }]
+  }
+};
